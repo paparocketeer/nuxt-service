72d665de6a8e759dd233bb1c7c8aabdb57b5ebf0
rocketjumpah <paparocketeer@gmail.com>
2020-08-06 14:45:48 +0300

    transitions

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,104 +1,30 @@
 <template>
-  <!-- <div class="slider">
-      <ul class="slider__container">
-        <li class="slider__slide">
-          <img
-            src="/image-styles/potolok/potolok4--small.jpg"
-            alt="Натяжные потолки в подарок"
-            srcset="/image-styles/potolok/potolok4--small.jpg 540w, /image-styles/potolok/potolok4--medium.jpg 1024w, /image-styles/potolok/potolok4--large.jpg 1920w"
-            sizes="100vw"
-            class="slider__image w-full h-75-screen lg:max-h-2xl"
-          />
+  <!-- <transition-group name="fade" tag="div">
+      <div v-for="i in [currentIndex]" :key="i">
+        <img :src="currentImg" />
+      </div>
+    </transition-group> -->
+  <div class="slider h-75-screen">
+        <!-- <ul class="slider__container"> -->
+          <transition-group name="fade" tag="ul" class="slider__container">
+          <li v-for="i in [currentIndex]" :key="i" class="slider__slide">
+            <img :src="currentImg" class="w-full h-75-screen lg:max-h-2xl slider__image"/>
           <div class="slider__overlay">
-            <div class="slider__content">
-              <p class="slider__title">Внимание акция!</p>
-              <p class="slider__text">При заказе ремонта – натяжные потолки в подарок!</p>
+            <div class="slider__content" :class="(currentIndex == i) ? 'is-visible' : ''">
+              <p class="slider__title" :class="(currentIndex == i) ? 'is-visible' : ''">Внимание акция!</p>
+              <p class="slider__text" :class="(currentIndex == i) ? 'is-visible' : ''">При заказе ремонта – натяжные потолки в подарок!</p>
             </div>
           </div>
         </li>
-        <li class="slider__slide" style="display: none;">
-          <img
-            src="/image-styles/home-slider/1--small.jpg"
-            alt="Высокое качество"
-            srcset="/image-styles/home-slider/1--small.jpg 540w, /image-styles/home-slider/1--medium.jpg 1024w, /image-styles/home-slider/1--large.jpg 1920w"
-            sizes="100vw"
-            class="slider__image w-full h-75-screen lg:max-h-2xl"
-          />
-          <div class="slider__overlay">
-            <div class="slider__content">
-              <p class="slider__title">Высокое качество</p>
-              <p class="slider__text">У нас более 8 лет опыта монтажно-отделочных работ</p>
-            </div>
-          </div>
-        </li>
-        <li class="slider__slide" style="display: none;">
-          <img
-            src="/image-styles/home-slider/2--small.jpg"
-            alt="Ремонт стен и полов"
-            srcset="/image-styles/home-slider/2--small.jpg 540w, /image-styles/home-slider/2--medium.jpg 1024w, /image-styles/home-slider/2--large.jpg 1920w"
-            sizes="100vw"
-            class="slider__image w-full h-75-screen lg:max-h-2xl"
-          />
-          <div class="slider__overlay">
-            <div class="slider__content">
-              <p class="slider__title">Гарантия</p>
-              <p class="slider__text">Гарантируем качество выполненных работ на срок от 5 лет</p>
-            </div>
-          </div>
-        </li>
-        <li class="slider__slide" style="display: none;">
-          <img
-            src="/image-styles/home-slider/3--small.jpg"
-            alt="Ремонт стен и полов"
-            srcset="/image-styles/home-slider/3--small.jpg 540w, /image-styles/home-slider/3--medium.jpg 1024w, /image-styles/home-slider/3--large.jpg 1920w"
-            sizes="100vw"
-            class="slider__image w-full h-75-screen lg:max-h-2xl"
-          />
-          <div class="slider__overlay">
-            <div class="slider__content">
-              <p class="slider__title">Оптимальная цена</p>
-              <p class="slider__text">Мы сэкономим Ваши деньги и предложим лучший ценовой диапазон</p>
-            </div>
-          </div>
-        </li>
-      </ul>
-      <button data-slide-prev class="slider__prev">Предыдущий слайд</button>
-      <button data-slide-next class="slider__next">Следующий слайд</button>
+        </transition-group>
+      <button data-slide-prev @click="prev()" class="slider__prev">Предыдущий слайд</button>
+      <button data-slide-next @click="next()" class="slider__next">Следующий слайд</button>
       <div class="slider__bullets">
         <button data-slide="0" class="slider__bullet slider__bullet--active">К слайду 0</button>
         <button data-slide="1" class="slider__bullet">К слайду 1</button>
         <button data-slide="2" class="slider__bullet">К слайду 2</button>
         <button data-slide="3" class="slider__bullet">К слайду 3</button>
       </div>
-  </div>-->
-  <div class="slider">
-    <!-- <agile :dots="false" class="slider__container">
-      <div v-for="n in 6" class="slide slider__slide" :class="'slide--' + n" v-bind:key="n">{{ n }}</div>
-    </agile>-->
-    <VueSlickCarousel v-bind="slickOptions" class="slider__container">
-      <div v-for="i in 5" :key="i" class="slider__slide">
-        <img :src="`./${i}.jpg`" class="w-full h-75-screen lg:max-h-2xl slider__image"/>
-        <div class="slider__overlay">
-            <div class="slider__content">
-              <p class="slider__title">Оптимальная цена</p>
-              <p class="slider__text">Мы сэкономим Ваши деньги и предложим лучший ценовой диапазон</p>
-            </div>
-          </div>
-      </div>
-      <template #prevArrow="arrowOption">
-        <button
-          class="slider__prev z-10"
-        >{{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}</button>
-      </template>
-      <template #nextArrow="arrowOption">
-        <button
-          class="slider__next z-10"
-        >{{ arrowOption.currentSlide }}/{{ arrowOption.slideCount }}</button>
-      </template>
-      <template #customPaging="page">
-        <button class="slider__bullet">{{ page }}</button>
-      </template>
-    </VueSlickCarousel>
   </div>
 </template>
 
@@ -107,16 +33,60 @@ export default {
   name: 'Carousel',
   data() {
     return {
-      slickOptions: {
-        slidesToShow: 1,
-        arrows: true,
-        draggable: true,
-        adaptiveHeight: true,
-        dots: true,
-        dotsClass: 'slider__bullets',
-      },
+      images: [
+        "./1.jpg",
+        "./2.jpg",
+        "./3.jpg",
+        "./4.jpg",
+        "./5.jpg"
+      ],
+      timer: null,
+      currentIndex: 0
+    };
+  },
+
+  // created: function() {
+  //   this.startSlide();
+  // },
+
+  methods: {
+    startSlide: function() {
+      this.timer = setInterval(this.next, 4000);
+    },
+
+    next: function() {
+      this.currentIndex += 1;
+    },
+    prev: function() {
+      this.currentIndex -= 1;
     }
   },
+
+  computed: {
+    currentImg: function() {
+      return this.images[Math.abs(this.currentIndex) % this.images.length];
+    }
+  }
+  // data() {
+  //   return {
+  //     currentSlide: 1,
+  //     maxSlides: 5,
+  //   }
+  // },
+  // methods: {
+  //   next(){
+  //     if (this.currentSlide == this.maxSlides) {
+  //       this.currentSlide = 1
+  //     }
+  //       this.currentSlide ++
+  //   },
+  //   prev(){
+  //     if (this.currentSlide == 1) {
+  //       this.currentSlide = this.maxSlides
+  //     }
+  //     this.currentSlide --
+  //   }
+  // }
 }
 </script>
 
